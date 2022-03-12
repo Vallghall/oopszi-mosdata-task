a7c90e4650f4d979a5885f46c2fd9729d184cbf1
Vallghall <romangusev2001@mail.ru>
2022-03-13 02:41:57 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express"
+
+const app = express()
+const port = process.env.port || 3000
+
+app.set('view engine', 'pug')
+app.use(express.static('./public'))
+
+app.get('/', (req, res) => {
+    res.render('index', {title: 'JS web-server', message: 'First Express Pug App'})
+})
+
+app.use((req, res) => {
+    res.type('text/plain')
+    res.status(404)
+    res.send("NOT FOUND")
+})
+
+app.listen(port, 'localhost', () => {console.log("Starting Express application")})
